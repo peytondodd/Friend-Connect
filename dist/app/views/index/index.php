ef80952c1dc32411300351e306e0e249367c66d4
thejasonxie <thejasonxie@gmail.com>
2018-03-29 17:44:11 -0400

    setup page fixes

--- dist/app/views/index/index.php
+++ dist/app/views/index/index.php
@@ -127,6 +127,8 @@
       <li>chat
       with each other all in real time</li>
     </ul>
+    <p>To make events happen in real time, Friend Connect uses the
+    long polling method (similar to Facebook) using Ajax.</p>
     <p>The technologies used were just HTML, CSS, Vanilla JS,
     Ajax, Bootstrap, PHP, MySQL, SASS, Gulp, Geolocation and Unsplash api.</p>
     <p>Logo icon made by Bogdan Rosu from <a target="_blank" href="https://www.flaticon.com/free-icon/camera-shutter_69386">www.flaticon.com</a></p>
@@ -137,58 +139,5 @@
   </div>
 
 </div>
-
-
-<!-- <h3 class="text-center">Log in</h3>
-<div class="row">
-  <div class="col-md-6 mx-auto">
-    <form class="" action="<?php echo URLROOT; ?>/login" method="post">
-      <div class="form-group">
-        <label for="email">Email: </label>
-        <input class="form-control" type="email" name="login_email" value="<?php echo isset($data['login_email']) ? $data['login_email'] : ''; ?>">
-      </div>
-      <div class="form-group">
-        <label for="password">Password: </label>
-        <input class="form-control" type="password" name="password" value="">
-      </div>
-      <input class="btn btn-success" type="submit" name="login" value="Log In">
-    </form>
-  </div>
-</div>
-
-<h1>Friend Connect</h1>
-<p>Connect with your friends</p>
-<h3 class="text-center">Register</h3>
-<div class="row">
-  <div class="col-md-6 mx-auto">
-    <form class="" action="<?php echo URLROOT; ?>/register" method="post">
-      <div class="form-row">
-        <div class="form-group col-md-6">
-          <label for="firstName">First name: </label>
-          <input class="form-control" type="text" name="first_name" placeholder="First name" value="<?php echo isset($data['first_name']) ? $data['first_name'] : ''; ?>">
-        </div>
-        <div class="form-group col-md-6">
-          <label for="lastName">Last name: </label>
-          <input class="form-control" type="text" name="last_name" placeholder="Last name" value="<?php echo isset($data['last_name']) ? $data['last_name'] : ''; ?>">
-        </div>
-      </div>
-      <div class="form-group">
-        <label for="email">Email: </label>
-        <input class="form-control" type="email" name="register_email" placeholder="Email" value="<?php echo isset($data['register_email']) ? $data['register_email'] : ''; ?>">
-      </div>
-      <div class="form-group">
-        <label for="password">Password: </label>
-        <input class="form-control" type="password" name="password" placeholder="Password">
-      </div>
-      <div class="form-group">
-        <label for="confirmPassword">Confirm Password: </label>
-        <input class="form-control" type="password" name="confirm_password" placeholder="Confirm password">
-      </div>
-
-      <input class="btn btn-success" type="submit" name="register_index" value="Register">
-    </form>
-  </div>
-</div> -->
-
 
 <?php require APPROOT . "/views/inc/footer.php"; ?>
--- dist/public/js/index.js
+++ dist/public/js/index.js
@@ -357,40 +357,41 @@ var loginPage = (function() {
 
 var profileSetupPage = (function() {
   var profileSetupPage = document.querySelector(".profileSetupPage-bg");
-  var page1 = profileSetupPage.querySelector(".profileSetupPage1");
-  var page2 = profileSetupPage.querySelector(".profileSetupPage2");
-  var page3 = profileSetupPage.querySelector(".profileSetupPage3");
-  //page 1 buttons
-  var setUpNow = page1.querySelector("input[name='setUpNow']");
-  //page 2
-  var imgSelect = page2.querySelector(".profileSetupPage2-profileSelector");
-  var currentProfileIMG = page2.querySelector(".profileSetupPage2-profileIMG");
-  var img_input = page2.querySelector("#img_input");
-  var psp2back = page2.querySelector(".psp2-back");
-  var nextBtn = page2.querySelector("input[name='next']");
-  var hiddenDefault = page2.querySelector("input[name='defaultProfile']");
-  var uploadMessage = page2.querySelector(".uploadMessage");
-  //page 3
-  var psp3back = page3.querySelector(".psp3-back");
+  if (profileSetupPage) {
+    var page1 = profileSetupPage.querySelector(".profileSetupPage1");
+    var page2 = profileSetupPage.querySelector(".profileSetupPage2");
+    var page3 = profileSetupPage.querySelector(".profileSetupPage3");
+    //page 1 buttons
+    var setUpNow = page1.querySelector("input[name='setUpNow']");
+    //page 2
+    var imgSelect = page2.querySelector(".profileSetupPage2-profileSelector");
+    var currentProfileIMG = page2.querySelector(".profileSetupPage2-profileIMG");
+    var img_input = page2.querySelector("#img_input");
+    var psp2back = page2.querySelector(".psp2-back");
+    var nextBtn = page2.querySelector("input[name='next']");
+    var hiddenDefault = page2.querySelector("input[name='defaultProfile']");
+    var uploadMessage = page2.querySelector(".uploadMessage");
+    //page 3
+    var psp3back = page3.querySelector(".psp3-back");
 
-  //page 1 button
-  setUpNow.addEventListener("click", pspNext);
-  //page 2
-  imgSelect.addEventListener("click", selectedIMG);
-  img_input.addEventListener("change", imgUpload);
-  psp2back.addEventListener("click", pspBack);
-  nextBtn.addEventListener("click", pspNext);
-  //page 3
-  psp3back.addEventListener("click", pspBack);
+    //page 1 button
+    setUpNow.addEventListener("click", pspNext);
+    //page 2
+    imgSelect.addEventListener("click", selectedIMG);
+    img_input.addEventListener("change", imgUpload);
+    psp2back.addEventListener("click", pspBack);
+    nextBtn.addEventListener("click", pspNext);
+    //page 3
+    psp3back.addEventListener("click", pspBack);
 
-  if (page1.style.display = "block") {
-    if (window.innerHeight <= 575) {
-      profileSetupPage.style.height = "675px";
-    } else {
-      profileSetupPage.style.height = "100vh";
+    if (page1.style.display = "block") {
+      if (window.innerHeight <= 575) {
+        profileSetupPage.style.height = "675px";
+      } else {
+        profileSetupPage.style.height = "100vh";
+      }
     }
   }
-
   //page 2 image selectors
   function selectedIMG(event) {
     var thisEvent;
